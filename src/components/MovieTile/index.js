2233472dddc9210a75f3f236b75173980403b903
Liam <liam@awaitingimage.com>
2018-10-24 08:24:50 +0100

    Updated to make the site a PWA.

--- src/components/MovieTile/index.js
+++ src/components/MovieTile/index.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./styles.css";
 import { BASE_IMAGE_URL } from "../../constants";
 import { Badge } from "reactstrap";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default class MovieTile extends Component {
   render() {
     return (
       <div className={"container"}>
-        <img
+        <LazyLoadImage
           src={BASE_IMAGE_URL + this.props.movie.poster_path}
           className={"image"}
           alt={this.props.movie.title}
